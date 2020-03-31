6cbaa26ee42186284101cbb7c2df12987a23f92b
Julian <31254706+juliansoh@users.noreply.github.com>
2020-03-31 12:57:59 -0700

    Seeding project
    Uploaded project state as of 3/31/2020

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\wa-doh-web-bot-84fb-Web-Deploy.pubxml -p:Password=vZ20Da6W8r85xNidZag9r74CBcZWDd68lyglpkchSpvpoRbAg8bDhEwEb0JN
+
